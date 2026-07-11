1b94cbd70c4959d151b3d1420b3739074410bc21
Rcperez5 <rcperez5@cougarnet.uh.edu>
2026-07-11 00:17:48 -0500

    Update scripts.js

--- mis3371/homework4/scripts.js
+++ mis3371/homework4/scripts.js
@@ -20,7 +20,9 @@
    Reference: w3schools.com/jsref/event_oninput.asp
    ============================================================ */
 
-
+function updateSlider(val) {
+    document.getElementById("rating-display").textContent = val;
+}
 
 
 /* ============================================================
